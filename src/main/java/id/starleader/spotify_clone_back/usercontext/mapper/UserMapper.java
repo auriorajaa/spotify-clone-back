4a63ab58ed590d3016af0aa722809bf127278a1c
Aurio Rajaa <riorajaa2018@gmail.com>
2024-08-17 10:11:26 +0700

    configure auth0

--- src/main/java/id/starleader/spotify_clone_back/usercontext/mapper/UserMapper.java
+++ src/main/java/id/starleader/spotify_clone_back/usercontext/mapper/UserMapper.java
@@ -4,7 +4,7 @@ import id.starleader.spotify_clone_back.usercontext.ReadUserDTO;
 import id.starleader.spotify_clone_back.usercontext.domain.User;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "string")
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
     ReadUserDTO readUserDTOToUser(User entity);
--- src/main/java/id/starleader/spotify_clone_back/usercontext/repository/UserRepository.java
+++ src/main/java/id/starleader/spotify_clone_back/usercontext/repository/UserRepository.java
@@ -3,8 +3,10 @@ package id.starleader.spotify_clone_back.usercontext.repository;
 import id.starleader.spotify_clone_back.usercontext.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
-
+    Optional<User> findOneByEmail(String email);
 
 }
